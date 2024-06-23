99d8ca8b347b415779af77196c68c059d5027de1
unknown <dakotahawkins0110@gmail.com>
2024-06-23 14:14:47 -0400

    early stopping first pass

--- mixin.py
+++ mixin.py
@@ -1,5 +1,8 @@
 import torch
 import torchviz
+import tempfile
+
+from torch.utils.data.dataloader import DataLoader
 
 
 class MixNet(torch.nn.Module):
@@ -21,35 +24,84 @@ class MixNet(torch.nn.Module):
 
     def fit(
         self,
-        data_loader,
+        data_loader: DataLoader,
         n_epochs: int = 1000,
         verbose: bool = True,
         verbose_step=200,
         track_loss=False,
+        early_stopping: bool = False,
+        test_loader: DataLoader | None = None,
     ):
         self.train()
-        device = self.get_device()
-        batch_loss = torch.zeros(len(data_loader))
-        for t in range(n_epochs):
-            for batch, (X, y) in enumerate(data_loader):
-                X, y = X.to(device), y.to(device)
+        self.verbose_ = verbose
+        self.track_loss_ = track_loss
+        self.fit_device_ = self.get_device()
+        self.batch_loss_ = torch.zeros(len(data_loader))
+        self.n_epochs_ = n_epochs
+        if not early_stopping:
+            self.__fit(data_loader, verbose_step)
+        else:
+            assert test_loader is not None
+            self.__early_stop_fit(data_loader, test_loader, verbose_step)
 
-                loss = self.__partial_fit(X, y)
+    def __log_loss(self, epoch: int, print_loss: bool):
+        if self.track_loss_ or self.verbose_:
+            loss_std, loss_mean = torch.std_mean(self.batch_loss_)
+            self.mean_loss_.append(loss_mean.item())
+            self.std_loss_.append(loss_std.item())
+            self.epochs_.append(epoch)
+            if print_loss and self.verbose_:
+                print(
+                    f"Loss: {loss_mean.item():>7f} [{epoch + 1:>5d}/{self.n_epochs_:>5d}]",
+                    flush=True,
+                )
 
-                if (track_loss or verbose) and t == 0 or (t + 1) % verbose_step == 0:
-                    batch_loss[batch] = loss
+    def __fit(self, data_loader: DataLoader, verbose_step: int):
+        for t in range(self.n_epochs_):
+            self.__batch_fit(
+                data_loader=data_loader, batch_idx=t, verbose_step=verbose_step
+            )
+            self.__log_loss(epoch=t, print_loss=t == 0 or (t + 1) % verbose_step == 0)
 
-            if t == 0 or (t + 1) % verbose_step == 0:
-                loss_std, loss_mean = torch.std_mean(batch_loss)
-                if verbose:
-                    print(
-                        f"Loss: {loss_mean.item():>7f} [{t + 1:>5d}/{n_epochs:>5d}]",
-                        flush=True,
-                    )
-                if track_loss:
-                    self.mean_loss_.append(loss_mean.item())
-                    self.std_loss_.append(loss_std.item())
-                    self.epochs_.append(t)
+    def __early_stop_fit(
+        self,
+        training_loader: DataLoader,
+        test_loader: DataLoader,
+        verbose_step: int,
+        step_size: int,
+    ):
+        best_loss = float("inf")
+        model_local = tempfile.TemporaryFile(suffix=".pt")
+        for t in range(self.n_epochs_):
+            self.__batch_fit(
+                data_loader=training_loader, batch_idx=t, verbose_step=verbose_step
+            )
+            if (t % step_size) == 0:
+                test_loss = self.test(test_loader)
+                if test_loss < best_loss:
+                    best_loss = test_loss
+                self.cache_model(model_local)
+        self.load_saved_model(model_local)
+
+    def cache_model(self, path: str):
+        torch.save(self.state_dict(), path)
+
+    def load_saved_model(self, path: str):
+        self.load_saved_model(torch.load(path))
+
+    def __batch_fit(
+        self, data_loader: DataLoader, batch_idx: int, verbose_step: int
+    ) -> torch.Tensor:
+        for batch, (X, y) in enumerate(data_loader):
+            X, y = X.to(self.fit_device_), y.to(self.fit_device_)
+            loss = self.__partial_fit(X, y)
+
+            if (
+                (self.track_loss_ or self.verbose_)
+                and batch_idx == 0
+                or (batch_idx + 1) % verbose_step == 0
+            ):
+                self.batch_loss_[batch] = loss.item()
 
     def __partial_fit(self, X: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
         y_pred = self(X)
@@ -67,28 +119,16 @@ class MixNet(torch.nn.Module):
         return self.loss(y_pred, y)
 
     def test(self, dataloader):
-        size = len(dataloader.dataset)
         n_batches = len(dataloader)
+        test_loss = 0
         self.eval()
-        test_score, test_loss = 0, 0
+
         device = self.get_device()
         with torch.no_grad():
             for X, y in dataloader:
                 X, y = X.to(device), y.to(device)
-                score, loss = self.__score(X, y)
-                test_score += score
-                test_loss += loss
-        print(
-            f"Test error: Accuracy: {100 * test_score / size:>0.1f}%, "
-            f"Avg loss: {test_loss / n_batches:>8f}"
-        )
-
-    def __score(self, X: torch.Tensor, y: torch.Tensor):
-        """private score function to avoid eval() + no_grad() overhead"""
-        y_pred = self(X)
-        loss = self.loss(y_pred, y).item()
-        score = (y_pred.argmax(1) == y).type(torch.float).sum().item() / len(y)
-        return score, loss
+                test_loss += self.__calculate_loss(self(X), y).item()
+        return test_loss / n_batches
 
     def draw_network(self):
         X = torch.randn((1, self.input_size_))
